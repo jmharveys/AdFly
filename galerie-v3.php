a25532d38e7221daf6a6396c232440bbe1eb8465
Harvey, Jonathan <JHarvey@lapresse.ca>
2014-03-17 11:33:16 -0400

    Début intégration multi - LEGAL - non final.

--- galerie-v3.php
+++ galerie-v3.php
@@ -29,7 +29,7 @@
 			<div class='flip' style='0px; width: 480px; height: 324px;'> 
 				
 				<div class='front' style='width: 480px; height: 324px;'>
-					<div id="logo" style="background-image: url('public/images/demo/tmr.jpg');"></div>
+					<div class="logo" style="background-image: url('public/images/demo/tmr.jpg');"></div>
 					<div style="z-index:0;overflow:hidden;width: 480px; height: 324px;position:absolute;">
 						<div class='gallery'>
 							<div class='scroller'>
@@ -41,8 +41,8 @@
 											<p class="subtitle">Cancun, Mexique</p>
 										</div>
 										<div>
-											<p class="caption">à partir de<br/>par pers</p>
-											<p class="price">1 358</p>
+											<p class="caption">à partir pers.</p>
+											<p class="price">99 999</p>
 										</div>
 									</div>
 								</div>
@@ -50,12 +50,12 @@
 									<div style='width: 480px; height: 246px; background-image: url("public/images/demo/maroc.jpg");'></div>
 									<div class="description">
 										<div>
-											<p class="title">Mazagan Beach Resort</p>
+											<p class="title">Mazagan<br/>Beach II</p>
 											<p class="subtitle">El Jadida, Maroc</p>
 										</div>
 										<div>
-											<p class="caption">à partir de eff</p>
-											<p class="price">99&nbsp;199</p>
+											<p class="caption">14 caractères<br/>2 lignes maxx.</p>
+											<p class="price">4&nbsp;199</p>
 										</div>
 									</div>	
 								</div>	
@@ -67,7 +67,7 @@
 											<p class="subtitle">Machu Picchu, Pérou</p>
 										</div>
 										<div>
-											<p class="caption">à partir de<br/>par pers</p>
+											<p class="caption">14 caractèress<br/>2 lignes maxxx</p>
 											<p class="price">1 699</p>
 										</div>
 									</div>	
@@ -76,11 +76,11 @@
 									<div style='width: 480px; height: 246px; background-image: url("public/images/demo/portugal.jpg");'></div>
 									<div class="description">
 										<div>
-											<p class="title">Rocha Hotel Apartamento</p>
+											<p class="title">Rocha Hotel Apartament</p>
 											<p class="subtitle">Praia da Rocha, Portugal</p>
 										</div>
 										<div>
-											<p class="caption">à partir de<br/>par pers</p>
+											<p class="caption">à partir de<br/>par pers.</p>
 											<p class="price">2 400</p>
 										</div>
 									</div>	
@@ -93,7 +93,7 @@
 											<p class="subtitle">Paris, France</p>
 										</div>
 										<div>
-											<p class="caption">à partir de<br/>par pers</p>
+											<p class="caption">à partir de<br/>par pers.</p>
 											<p class="price">99,99</p>
 										</div>
 									</div>	
@@ -111,28 +111,144 @@
 						</div>
 					</div>
 				</div>
-				
 				<div class='back' style='width: 480px; height: 324px;'>
 					<div style="z-index:0;overflow:hidden;width: 480px; height:324px;position:absolute;">
 						<div class='gallery'>
 							<div class='scroller'>
 								<div>
+									<div class="close"></div>
 									<div class="description">
 										<div>
-											<p class="title">22 caractères maximum<br/>3 lignes pour el bloc total</p>
+											<p class="title">22 caractères maximumm<br/>3 lignes maximumm mmmm</p>
 											<p class="subtitle">16 caractères max.</p>
 										</div>
 										<div>
-											<p class="caption">14 caractèress<br/>2 lignes maxxx</p>
+											<p class="caption">14 caractèress</p>
 											<p class="price">99 999</p>
 										</div>
-										<div style="float:left;">Actually twee readymade pickled post-ironic, umami lomo selvage put a bird on it next level pop-up asymmetrical. Butcher post-ironic cray Cosby sweater, locavore authentic sustainable quinoa meggings next level retro kale chips Williamsburg. Skateboard Schlitz forage, fanny pack cred wolf fap pour-over. Lo-fi nihil non artisan accusamus. Artisan pariatur nesciunt nulla officia farm-to-table sriracha. You probably haven't heard of them et cliche sunt. Drinking vinegar dreamcatcher photo booth, plaid cray laboris irony bespoke biodiesel mumblecore seitan twee nihil delectus wayfarers.</div>
+										<ul class="rating">
+											<li></li>
+											<li></li>
+											<li></li>
+											<li></li>
+										</ul>
+										<div class="infos">Actually twee readymade pickled post-ironic, umami lomo selvage put a bird on it next level pop-up asymmetrical. Butcher post-ironic cray Cosby sweater, locavore authentic sustainable quinoa meggings next level retro kale chips Williamsburg. Skateboard Schlitz forage, fanny pack cred wolf fap pour-over. Lo-fi nihil non artisan accusamus. Artisan pariatur nesciunt nulla officia.</div>
+									
+									<div class="logo" style="background-image: url('public/images/demo/tmr.jpg');"></div>
+									<a class="btn-plusWeb noFlip" href="http://www.lapresse.ca"></a>
+									</div>
+									<div class='lp-legal-bg noFlip'></div>
+									<div class='lp-legal noFlip'>
+										<div class='lp-legal-btn noFlip'>Légal</div>
+										<div class='lp-legal-text noFlip'>© Mercedes-Benz Canada Inc., 2014. Les offres peuvent changer sans préavis et ne peuvent être jumelées à d’autres offres. Pour connaître les détails, voyez votre concessionnaire Mercedes-Benz West Island. L’offre est valide du 17 au 19 mars seulement.</div>
 									</div>
 								</div>
-								<div>2</div>	
-								<div>3</div>	
-								<div>4</div>	
-								<div>5</div>	
+								<div>
+									<div class="close"></div>
+									<div class="description">
+										<div>
+											<p class="title">Mazagan<br/>Beach II</p>
+											<p class="subtitle">El Jadida, Maroc</p>
+										</div>
+										<div>
+											<p class="caption">14 caractèress<br/>2 lignes maxxx</p>
+											<p class="price">4&nbsp;199</p>
+										</div>
+										<ul class="rating">
+											<li></li>
+											<li></li>
+											<li></li>
+										</ul>
+										<div class="infos">Actually twee readymade pickled post-ironic, umami lomo selvage put a bird on it next level pop-up asymmetrical. Butcher post-ironic cray Cosby sweater, locavore authentic sustainable quinoa meggings next level retro kale chips Williamsburg. Skateboard Schlitz forage, fanny pack cred wolf fap pour-over. Lo-fi nihil non artisan accusamus. Artisan pariatur nesciunt nulla officia.</div>
+									
+									<div class="logo" style="background-image: url('public/images/demo/tmr.jpg');"></div>
+									<a class="btn-plusWeb noFlip" href="http://www.lapresse.ca"></a>
+									</div>
+									<div class='lp-legal-bg noFlip'></div>
+									<div class='lp-legal noFlip'>
+										<div class='lp-legal-btn noFlip'>Légal</div>
+										<div class='lp-legal-text noFlip'>© Mercedes-Benz Canada Inc., 2014. Les offres peuvent changer sans préavis et ne peuvent être jumelées à d’autres offres. Pour connaître les détails, voyez votre concessionnaire Mercedes-Benz West Island. L’offre est valide du 17 au 19 mars seulement.</div>
+									</div>
+								</div>	
+								<div>
+									<div class="close"></div>
+									<div class="description">
+										<div>
+											<p class="title">Machu Picchu Lodge 123</p>
+											<p class="subtitle">Machu Picchu, Pérou</p>
+										</div>
+										<div>
+											<p class="caption">14 caractères<br/>2 lignes maxx.</p>
+											<p class="price">1 699</p>
+										</div>
+										<ul class="rating">
+											<li></li>
+											<li></li>
+											<li></li>
+											<li></li>
+										</ul>
+										<div class="infos">Actually twee readymade pickled post-ironic, umami lomo selvage put a bird on it next level pop-up asymmetrical. Butcher post-ironic cray Cosby sweater, locavore authentic sustainable quinoa meggings next level retro kale chips Williamsburg. Skateboard Schlitz forage, fanny pack cred wolf fap pour-over. Lo-fi nihil non artisan accusamus. Artisan pariatur nesciunt nulla officia.</div>
+									
+									<div class="logo" style="background-image: url('public/images/demo/tmr.jpg');"></div>
+									<a class="btn-plusWeb noFlip" href="http://www.lapresse.ca"></a>
+									</div>
+									<div class='lp-legal-bg noFlip'></div>
+									<div class='lp-legal noFlip'>
+										<div class='lp-legal-btn noFlip'>Légal</div>
+										<div class='lp-legal-text noFlip'>© Mercedes-Benz Canada Inc., 2014. Les offres peuvent changer sans préavis et ne peuvent être jumelées à d’autres offres. Pour connaître les détails, voyez votre concessionnaire Mercedes-Benz West Island. L’offre est valide du 17 au 19 mars seulement.</div>
+									</div>
+								</div>	
+								<div>
+									<div class="close"></div>
+									<div class="description">
+										<div>
+											<p class="title">Rocha Hotel Apartament</p>
+											<p class="subtitle">Praia, Portugal</p>
+										</div>
+										<div>
+											<p class="caption">à partir de<br/>par pers.</p>
+											<p class="price">2 400</p>
+										</div>
+										<ul class="rating">
+											<li></li>
+										</ul>
+										<div class="infos">Actually twee readymade pickled post-ironic, umami lomo selvage put a bird on it next level pop-up asymmetrical. Butcher post-ironic cray Cosby sweater, locavore authentic sustainable quinoa meggings next level retro kale chips Williamsburg. Skateboard Schlitz forage, fanny pack cred wolf fap pour-over. Lo-fi nihil non artisan accusamus. Artisan pariatur nesciunt nulla officia.</div>
+									
+									<div class="logo" style="background-image: url('public/images/demo/tmr.jpg');"></div>
+									<a class="btn-plusWeb noFlip" href="http://www.lapresse.ca"></a>
+									</div>
+									<div class='lp-legal-bg noFlip'></div>
+									<div class='lp-legal noFlip'>
+										<div class='lp-legal-btn noFlip'>Légal</div>
+										<div class='lp-legal-text noFlip'>© Mercedes-Benz Canada Inc., 2014. Les offres peuvent changer sans préavis et ne peuvent être jumelées à d’autres offres. Pour connaître les détails, voyez votre concessionnaire Mercedes-Benz West Island. L’offre est valide du 17 au 19 mars seulement.</div>
+									</div>
+								</div>	
+								<div>
+									<div class="close"></div>
+									<div class="description">
+										<div>
+											<p class="title">Effeil Capitol Hotel</p>
+											<p class="subtitle">Paris, France</p>
+										</div>
+										<div>
+											<p class="caption">à partir de<br/>par pers.</p>
+											<p class="price">99,99</p>
+										</div>
+										<ul class="rating">
+											<li></li>
+											<li></li>
+										</ul>
+										<div class="infos">Actually twee readymade pickled post-ironic, umami lomo selvage put a bird on it next level pop-up asymmetrical. Butcher post-ironic cray Cosby sweater, locavore authentic sustainable quinoa meggings next level retro kale chips Williamsburg. Skateboard Schlitz forage, fanny pack cred wolf fap pour-over. Lo-fi nihil non artisan accusamus. Artisan pariatur nesciunt nulla officia.</div>
+									
+									<div class="logo" style="background-image: url('public/images/demo/tmr.jpg');"></div>
+									<a class="btn-plusWeb noFlip" href="http://www.lapresse.ca"></a>
+									</div>
+									<div class='lp-legal-bg noFlip'></div>
+									<div class='lp-legal noFlip'>
+										<div class='lp-legal-btn noFlip'>Légal</div>
+										<div class='lp-legal-text noFlip'>© Mercedes-Benz Canada Inc., 2014. Les offres peuvent changer sans préavis et ne peuvent être jumelées à d’autres offres. Pour connaître les détails, voyez votre concessionnaire Mercedes-Benz West Island. L’offre est valide du 17 au 19 mars seulement.</div>
+									</div>
+								</div>
 							</div>
 							<div class='pager-wrapper'>
 								<ul class='pager'>
@@ -142,13 +258,20 @@
 									<li>3</li>
 									<li>4</li>
 								</ul>
-							</div>					
+							</div>	
+											
 						</div>
 					</div>
 				</div>
 		<!--=== RETOURNER html | fin ==============-->
 	</div>
+
+
+
 </div>
+
+	
+
 </div>
 	<script src="<?= URL ?>public/scripts/min/mustache.min.js"></script>
 	<script src="<?= URL ?>public/scripts/min/iscroll5.min.js"></script>
@@ -169,8 +292,12 @@
 
 			}
 			if ( elem.classList.contains('active') && body.classList.contains('flipped') && !(body.classList.contains('moving')) ) { 
-					elem.classList.remove('active'); 
-					body.classList.remove('flipped'); 
+					if (  !(event.target.classList.contains('noFlip'))  ) {
+						elem.classList.remove('active'); 
+						body.classList.remove('flipped'); 
+					} else {
+						body.classList.add('legalOpen');
+					}
 			} 
 		}
 
@@ -216,7 +343,9 @@
 				var bullet = cleanWhitespace(el2).childNodes[currentPage];
 				bullet.classList.add('selected');
 			});			
-			gallery.goToPage(this.currentPage.pageX, 0, 0);
+			Array.prototype.forEach.call(myGallerys, function(el) {
+		    	el.goToPage(currentPage, 0, 0);
+			});		
 		}
 
 		function scrollStart() {
@@ -225,6 +354,7 @@
 
 		var gallery; 
 		var wrapper = document.getElementsByClassName('gallery');
+		var myGallerys = new Array();
 
 		function initGallery(elem) {
 		    gallery = new IScroll(elem, { 
@@ -234,10 +364,13 @@
 				momentum: false, 
 				hScrollbar: false,
 				snapThreshold: 10,
-				tap: true
+				tap: true,
+				click: true 
+				// Activé seulement pour desktop afin de rendre les liens cliquable
 			});
 			gallery.on('scrollEnd', endScroll);
 			gallery.on('scrollStart', scrollStart);
+			myGallerys.push(gallery);
 		}
 
 		Array.prototype.forEach.call(wrapper, function(el) {
@@ -261,6 +394,64 @@
 			}
 			body.classList.add('lp-loaded');
 		}
+
+
+		var legalList = document.getElementsByClassName('lp-legal');
+		var legalArray = [];
+		// var doc = new Array();
+		
+		for (var i = 0; i < legalList.length; ++i) {
+			// console.log();
+			console.log(cleanWhitespace(legalList[i]).childNodes);
+			// doc.push(cleanWhitespace(legalList[i]).childNodes);
+			// var notes = null;
+			// console.log(doc.length);
+			// for (var z = 0; z < doc.length; z++) {
+			// 	// console.log(doc);
+			//     if (doc.childNodes[z].className == "lp-legal-btn") {
+			//       btn = doc.childNodes[z];
+			//       break;
+			//     }        
+			// }
+			// console.log(btn);
+
+
+
+
+
+
+			
+
+	  //   	legalArray[i] = {
+			// 	container: legalList[i],
+			// 	btn: document.getElementsByClassName('lp-legal-btn')[0],
+			// 	bg: document.getElementsByClassName('lp-legal-bg')[0],
+			// 	animate: function() {
+			// 		if(legalArray[i].bg.classList.contains('lp-legal-active')) {
+			// 			body.classList.remove('legalOpen');
+			// 			// legalArray[i].container.classList.remove('lp-legal-active');
+			// 			// legalArray[i].bg.classList.remove('lp-legal-active');
+			// 		} else {
+			// 			// body.bg.classList.classList.add('legalOpen');
+			// 			// legalArray[i].container.classList.add('lp-legal-active');
+			// 			// legalArray[i].bg.classList.add('lp-legal-active');
+			// 		}
+			// 	}
+			// }
+			// legalArray[i].btn.onclick = function() {legalArray[i].animate()};
+			// legalArray[i].bg.onclick = function() {legalArray[i].animate()};
+		}
+
+
+
+
+
+		// Array.prototype.forEach.call(legalList, function(el) {
+		//     initLegals(el);
+		// });		
+
+
+		
 	</script>
 </body>
 </html>
